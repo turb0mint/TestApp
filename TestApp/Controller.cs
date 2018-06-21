eb4a6613bf9387a728ff53002d0930fe2ea70d4f
turb0mint <19749694+turb0mint@users.noreply.github.com>
2018-06-21 01:55:36 +0100

    Add project files.

---
+++ TestApp/Controller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    
+    enum STATE { NotInitalised,Running,DownToProduction};
+    
+
+
+    class Controller
+    {
+        public event EventHandler<STATE> StateChangeEvent;
+        private Screen S1;
+        private STATE _State;
+        public STATE State { get { return _State; } }
+
+        public Controller(Screen S1)
+        {
+            this.S1 = S1;
+                        S1.output("Controller created");
+        }
+
+        public async Task Initalize()
+        {
+            if (_State != STATE.Running)
+            {
+              await Task.Run( () =>  Thread.Sleep(5000));
+                _State = STATE.Running;
+                StateChangeEvent?.Invoke(this, STATE.Running);
+            } else
+            {
+               // S1.output("Controller is already Running");
+            }
+        }
+        public void ShutDown()
+        {
+            _State = STATE.DownToProduction;
+             S1.output("Controller DownToProduction");
+        }
+
+        
+
+
+
+
+
+
+
+
+    }
+}
---
+++ TestApp/Screen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Screen : Form
+    {
+        Controller Cntrl;
+        
+        public Screen()
+        {
+            InitializeComponent();
+            Cntrl= new Controller(this);
+            Cntrl.StateChangeEvent += Cntrl_StateChangeEvent;
+
+        }
+
+        private void Cntrl_StateChangeEvent(object sender, STATE e)
+        {
+            this.output(e.ToString());
+        }
+
+        public void output(string message)
+        {
+
+            textBox1.AppendText(message);
+            textBox1.AppendText(Environment.NewLine);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            this.button1.Enabled = false;
+            this.button1.Text = "Starting...";
+          await Cntrl.Initalize();
+            this.button1.Enabled = true;
+            this.button1.Text = "START";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cntrl.ShutDown();
+        }
+    }
+}
